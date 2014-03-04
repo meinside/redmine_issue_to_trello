7e9cc12d01f8dda4e38b0f3fa0a730e2045acf08
Lucas Jenss <lucas@x3ro.de>
2014-03-04 15:13:51 +0100

    Don't catch exceptions on Trello card creation
    This seems to be preferable since, with the previous
    solution, it wasn't immediately obvious when Trello
    card creation failed. As soon as somebody notices one
    could look at the logfiles, but by then dozens of issues
    might've already been created.

--- lib/hooks.rb
+++ lib/hooks.rb
@@ -14,27 +14,23 @@ module RedmineTrello
 
   class Hooks < Redmine::Hook::ViewListener
     def controller_issues_new_after_save(context = {})
-      begin
-        # read config
-        config = TrelloHelper.config
-        app_key = config["app_key"]
-        user_token = config["user_token"]
-        list_id = config["list_id"]
+      # read config
+      config = TrelloHelper.config
+      app_key = config["app_key"]
+      user_token = config["user_token"]
+      list_id = config["list_id"]
 
-        # read context for the issue
-        project_name = context[:project].name
-        issue_subject = context[:issue].subject
-        issue_description = context[:issue].description
-        issue_duedate = context[:issue].due_date
+      # read context for the issue
+      project_name = context[:project].name
+      issue_subject = context[:issue].subject
+      issue_description = context[:issue].description
+      issue_duedate = context[:issue].due_date
 
-        card_formatter = CardFormatter.new(context[:project], context[:issue])
+      card_formatter = CardFormatter.new(context[:project], context[:issue])
 
-        # post a new card
-        trello = TrelloHelper.authenticate(app_key, user_token)
-        trello.post(list_id, card_formatter.title, card_formatter.description)
-      rescue
-        Rails.logger.error "While sending Redmine issue to Trello: #{$!}"
-      end
+      # post a new card
+      trello = TrelloHelper.authenticate(app_key, user_token)
+      trello.post(list_id, card_formatter.title, card_formatter.description)
     end
   end
 
